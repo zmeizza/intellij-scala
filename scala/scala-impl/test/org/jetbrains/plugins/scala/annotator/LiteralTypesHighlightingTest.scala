cdc9539e75e098f5f8ced2307116163087b9c766
jamesbrain <marso.des@gmail.com>
2018-04-06 15:46:03 +0300

    Actually conform literal types to right-hand undefined types #SCL-11193 In Progress

--- scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/LiteralTypesHighlightingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/LiteralTypesHighlightingTest.scala
@@ -166,4 +166,6 @@ class LiteralTypesHighlightingTest extends LiteralTypesHighlightingTestBase {
   def testWithSpaces(): Unit = doTest()
 
   def testExtendsSingleton(): Unit = doTest()
+
+  def testSome(): Unit = doTest()
 }
