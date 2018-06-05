658e1fe799680727d8a26992680016c513998010
Roman Shein <marso.des@gmail.com>
2018-06-05 21:17:21 +0300

    Initial implementation of scalafmt as formatter replacement support
    #SCL-13658 in progress

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -88,6 +88,13 @@ object Dependencies {
 
   val scalastyle: ModuleID = "org.scalastyle" %% "scalastyle" % "1.0.0"
   val scalariform: ModuleID = "org.scalariform" %% "scalariform" % "0.2.2"
+  val scalafmt: Seq[ModuleID] = Seq(
+    "com.geirsson" %% "scalafmt-core" % "1.5.1",
+    "com.geirsson" %% "metaconfig-core" % "0.4.0",
+    "com.geirsson" %% "metaconfig-typesafe-config" % "0.4.0",
+    "com.typesafe" % "config" % "1.2.1",
+    "com.lihaoyi" %% "sourcecode" % "0.1.3"
+  )
   val macroParadise: ModuleID = "org.scalameta" % "paradise" % paradiseVersion cross CrossVersion.full
   val scalaMetaCore: ModuleID = "org.scalameta" %% "scalameta" % scalaMetaVersion withSources() exclude("com.google.protobuf", "protobuf-java")
   val fastparse: ModuleID = "com.lihaoyi" % s"fastparse_$scalaBinaryVersion" % "0.4.3" // transitive dependency of scalaMeta, needs explicit versioning
@@ -131,7 +138,7 @@ object DependencyGroups {
     jamm,
     scalastyle,
     scalariform
-  )
+  ) ++ scalafmt
 
   val decompiler = Seq(
     scalaLibrary,
